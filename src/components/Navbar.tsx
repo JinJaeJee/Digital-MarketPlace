e1857e31c2e092258603febf49621151091e6a3c
JinJaeJee <jpluemphiromnat@gmail.com>
2023-12-03 20:43:47 +0700

    feat: nav V.2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import { MaxWidthWrapper } from "./MaxWidthWrapper";
+import { Icons } from "./icon";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
@@ -6,7 +9,18 @@ const Navbar = () => {
       <header className="relative bg-white">
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
-            <div className="flex h-16 "></div>
+            <div className="flex h-16 item-center">
+              {/* Todo Moblie Device*/}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10 mt-3" />
+                </Link>
+              </div>
+
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
       </header>
---
+++ src/config/index.tsx
@@ -0,0 +1,11 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI_Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+      },
+    ],
+  },
+];
