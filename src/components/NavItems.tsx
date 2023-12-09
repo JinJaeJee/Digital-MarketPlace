af09bd30b99162c8ab4da7416ab1f60b5969bb58
TobaFall01 <jpluemphiromnat@gmail.com>
2023-12-09 21:26:08 +0700

    feat: nav v2

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,40 @@
 "use client";
 
-import { useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useRef, useState } from "react";
+import Navitem from "./Navitem";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-  return <div className="flex gap-4 h-full">ZZZdddZZ1</div>;
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>();
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+        return (
+          <Navitem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
